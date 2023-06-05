5eafd76e71b910266fcd4ebb065af7c4181e44d4
Dereje Seifu <derejeseifu3030@gmail.com>
2023-06-05 23:37:43 +0300

    updated

--- components/TheFooter.jsx
+++ components/TheFooter.jsx
@@ -7,59 +7,49 @@ import { IoLogoYoutube } from '@react-icons/all-files/io/IoLogoYoutube'
 import { HiOutlineMail } from '@react-icons/all-files/hi/HiOutlineMail'
 import { FaLinkedinIn } from '@react-icons/all-files/fa/FaLinkedinIn'
 
-// import { BsFacebook, BsTelegram, BsInstagram, BsTiktok, BsYoutube } from 'react-icons/bs'
-// import { HiOutlineMail,FaLinkedinIn } from 'react-icons/ai'
-
-
 import Section from './Section'
 import Link from 'next/link'
  const TheFooter = () => {
   return (   
-    <footer className="max-w-full bg-primary pt-10 lg:pt-20">
+    <footer className="max-w-full bg-primary-green pt-10 lg:pt-20">
           <Section>
               <div className='text-center text-white text-xl lg:text-3xl font-bold'>Follow Us On</div>
-              <div className='hidden lg:flex flex-row justify-center gap-10 mt-14 lg:mt-20'>
-              <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-800 transition ease-in-out delay-150  hover:rotate-12  duration-1000 '> <FaFacebookSquare size={50}  className='text-white' /></a>                  
-              <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-400 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <ImTelegram size={50} className='text-white' /></a>                  
-              <a href="/" className='rounded-full p-2 lg:p-5 bg-black transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <FaInstagramSquare size={50} className='text-white' /></a>                    
-             <a href="/" className='rounded-full p-2 lg:p-5 bg-red-500 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <HiOutlineMail size={50} className='text-white' /></a>                
-            <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-600 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <FaLinkedinIn size={50} className='text-white' /></a>                  
-            <a href="/" className='rounded-full p-2 lg:p-5 bg-black transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <SiTiktok size={50} className='text-white' /></a>
-            <a href="/" className='rounded-full p-2 lg:p-5 bg-red-600 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <IoLogoYoutube size={50} className='text-white' /></a>
-
-                  
-              </div>
-              
-              <div className='w-full flex flex-row justify-center lg:hidden gap-2 mt-5'>
-              <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-800 transition ease-in-out delay-150  hover:rotate-12  duration-1000 '> <FaFacebookSquare size={20}  className='text-white' /></a>
-              <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-400 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <ImTelegram size={20} className='text-white' /></a>
-              <a href="/" className='rounded-full p-2 lg:p-5 bg-black transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <FaInstagramSquare size={20} className='text-white' /></a>
-              <a href="/" className='rounded-full p-2 lg:p-5 bg-red-500 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <HiOutlineMail size={20} className='text-white' /></a>
-              <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-600 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <FaLinkedinIn size={20} className='text-white' /></a>
-              <a href="/" className='rounded-full p-2 lg:p-5 bg-black transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <SiTiktok size={20} className='text-white' /></a>
-              <a href="/" className='rounded-full p-2 lg:p-5 bg-red-600 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <IoLogoYoutube size={20} className='text-white' /></a>
-              </div>
-                
+                <div className='hidden lg:flex flex-row justify-center gap-10 mt-14 lg:mt-20'>
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-800 transition ease-in-out delay-150  hover:rotate-12  duration-1000 '> <FaFacebookSquare size={50}  className='text-white' /></a>                  
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-400 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <ImTelegram size={50} className='text-white' /></a>                  
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-black transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <FaInstagramSquare size={50} className='text-white' /></a>                    
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-red-500 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <HiOutlineMail size={50} className='text-white' /></a>                
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-600 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <FaLinkedinIn size={50} className='text-white' /></a>                  
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-black transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <SiTiktok size={50} className='text-white' /></a>
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-red-600 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <IoLogoYoutube size={50} className='text-white' /></a>
+                </div>
+                <div className='w-full flex flex-row justify-center lg:hidden gap-2 mt-5'>
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-800 transition ease-in-out delay-150  hover:rotate-12  duration-1000 '> <FaFacebookSquare size={20}  className='text-white' /></a>
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-400 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <ImTelegram size={20} className='text-white' /></a>
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-black transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <FaInstagramSquare size={20} className='text-white' /></a>
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-red-500 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <HiOutlineMail size={20} className='text-white' /></a>
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-blue-600 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <FaLinkedinIn size={20} className='text-white' /></a>
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-black transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <SiTiktok size={20} className='text-white' /></a>
+                    <a href="/" className='rounded-full p-2 lg:p-5 bg-red-600 transition ease-in-out delay-150  hover:rotate-12  duration-1000'> <IoLogoYoutube size={20} className='text-white' /></a>
+                </div>        
           </Section>
           <div className='grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-32 w-full mx-auto px-5 lg:px-20 mt-10 lg:mt-14'>
-              <Section>
+              <Section classNam="w-full">
                   <div className='text-white text-2xl'>Address One</div>
                   <div className='text-white text-xl'>1. Bole Street Addis Ababa, Ethiopia</div>
                   <div className='text-white text-xl'>+251920907878    +251910813571</div>
                   <div className='mt-10 lg:mt-14'>
                       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252226.0134836966!2d38.4836695395734!3d8.969248113153492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b84f21691961f%3A0x822a438ed5adcc15!2sAddis%20Ababa%2C%20Ethiopia!5e0!3m2!1sen!2snl!4v1684613347486!5m2!1sen!2snl" className='w-full h-96' style={{ border: 0 }} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-              </div>
+                  </div>
               </Section>
-               <Section>
+              <Section classNam="w-full">
                   <div className='text-white text-2xl'>Address Two</div>
                   <div className='text-white text-xl'>2. 7523 24th Ave SW Seattle, Washington</div>
                   <div className='text-white text-xl'>206-353-5373</div>
                   <div className='mt-10 lg:mt-14'>
                       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252226.0134836966!2d38.4836695395734!3d8.969248113153492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b84f21691961f%3A0x822a438ed5adcc15!2sAddis%20Ababa%2C%20Ethiopia!5e0!3m2!1sen!2snl!4v1684613347486!5m2!1sen!2snl" className='w-full h-96' style={{ border: 0 }} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                   </div>
-                  
               </Section>
-              
           </div>
           <div className='w-full bg-white text-center mt-14 pt-6 pb-6'>Copyright © 2023 | Developed by Merahi Technologies</div>
 </footer>
--- components/WhyChooseUs.jsx
+++ components/WhyChooseUs.jsx
@@ -11,59 +11,61 @@ import convenence from '@public/convenience.png'
 import Section from './Section'
 const WhyChooseUs = () => {
   return (
-    <div className='grid grid-cols-2 md:grid-cols-4 gap-4 lg:gap-20 py-5  lg:py-20 w-11/12 mx-auto'>
+    <div>
+      <div className='flex justify-center items-center mt-10'>
+        <h4 className='font-satoshi text-2xl font-bold'>WHY YOU CHOOSE US</h4>
+      </div>
+      <div className='grid grid-cols-2 md:grid-cols-4 gap-4 lg:gap-20 py-5  lg:py-20 w-11/12 mx-auto'>
+        <div className='flex justify-center'>
+          <Section className='flex flex-col justify-center items-center'>
+            <Image src={honesty} alt="honest icon" style={{maxWidth:60}} className='backdrop-blur' />
+            <div className='lg:mt-5 text-lg'>Honesty</div>
+          </Section>
+        </div>
+        <div className='flex justify-center'>
+        <Section>          
+          <Image src={accountability} alt="accountability icon" style={{maxWidth:60}} />
+          <div className='lg:mt-5 text-lg'>Accountablity</div>
         
-    <div className='flex justify-center'>
-      <Section className='flex flex-col justify-center items-center'>
-        <Image src={honesty} alt="honest icon" style={{maxWidth:60}} className='backdrop-blur' />
-        <div className='lg:mt-5 text-lg'>Honesty</div>
-      </Section>
-    </div>
-    <div className='flex justify-center'>
-    <Section>          
-       <Image src={accountability} alt="accountability icon" style={{maxWidth:60}} />
-      <div className='lg:mt-5 text-lg'>Accountablity</div>
-     
-      </Section>
-    </div>
-      <div className='flex justify-center'>
-    <Section className='mt-10 lg:mt-0'>
-        <Image src={affordable} alt="affordable icon" style={{maxWidth:60}} />
-        <div className='lg:mt-5'>Affordable</div>
-        </Section>
+          </Section>
+        </div>
+        <div className='flex justify-center'>
+          <Section className='mt-10 lg:mt-0'>
+            <Image src={affordable} alt="affordable icon" style={{maxWidth:60}} />
+            <div className='lg:mt-5'>Affordable</div>
+          </Section>
+        </div>
+        <div className='flex justify-center'>
+          <Section className='mt-10 lg:mt-0'>
+            <Image src={accessibility} alt="accessibility icon" style={{maxWidth:60}} />
+            <div className='lg:mt-5 text-lg'>Accessible</div>
+          </Section>
+        </div>
+        <div className='flex justify-center'>
+          <Section>
+            <Image src={punctul} alt="panctual icon" style={{maxWidth:60}} />
+            <div className='lg:mt-5 text-lg'>Punctuality</div>
+          </Section>
+        </div>
+        <div className='flex justify-center'>
+          <Section>
+            <Image src={convenence} alt="convenence icon" style={{maxWidth:60}} />
+            <div className='lg:mt-5 text-lg'>Convenience</div>
+          </Section>
+        </div>
+        <div className='flex justify-center'>
+          <Section className='mt-10 lg:mt-0'>
+            <Image src={acridit} alt="acridit icon"  style={{maxWidth:60}}/>
+            <div className='lg:mt-5 text-lg text-center'>Accredited</div>
+          </Section>
+        </div>
+        <div className='flex justify-center'>
+          <Section className='mt-10 lg:mt-0'>
+            <Image src={integrit} alt="convenence icon" style={{maxWidth:60}} />
+            <div className='lg:mt-5 text-lg text-center'>Integrit</div>
+          </Section>
+        </div>
       </div>
-      <div className='flex justify-center'>
-    <Section className='mt-10 lg:mt-0'>
-    <Image src={accessibility} alt="accessibility icon" style={{maxWidth:60}} />
-      <div className='lg:mt-5 text-lg'>Accessible</div>
-        </Section>
-      </div>
-      <div className='flex justify-center'>
-  <Section>
-  <Image src={punctul} alt="panctual icon" style={{maxWidth:60}} />
-
-    <div className='lg:mt-5 text-lg'>Punctuality</div>
-        </Section>
-      </div>
-      <div className='flex justify-center'>
-  <Section>
-  <Image src={convenence} alt="convenence icon" style={{maxWidth:60}} />
-    <div className='lg:mt-5 text-lg'>Convenience</div>
-        </Section>
-      </div>
-      <div className='flex justify-center'>
-  <Section className='mt-10 lg:mt-0'>
-  <Image src={acridit} alt="acridit icon"  style={{maxWidth:60}}/>
-    <div className='lg:mt-5 text-lg text-center'>Accredited</div>
-        </Section>
-      </div>
-      <div className='flex justify-center'>
-  <Section className='mt-10 lg:mt-0'>
-  <Image src={integrit} alt="convenence icon" style={{maxWidth:60}} />
-    <div className='lg:mt-5 text-lg text-center'>Integrit</div>
-        </Section>
-      </div>
-      
     </div>
   )
 }
